9a9609b204db95bfe75f0f3f002568afa925d1bf
atqrhmn <atiqur.rahman31951@gmail.com>
2025-01-16 00:34:43 +0600

    Time: 0 ms (100%), Space: 44.2 MB (5.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> lr, rl, ans;
+        vector<int> rl, ans;
         int len = nums.size() - 1;
         int j = len, rlMul = 1, lrMul = 1;
 
         for(int i = 0; i <= len; i++) {
-            lr.push_back(1);
             rl.push_back(1);
         }
 
-        for(int i = 0; i <= len; i++) {
-            lr[i] = lrMul * nums[i];
-            rl[j] = rlMul * nums[j];
-
-            lrMul = lr[i];
-            rlMul = rl[j];
-            j--;
+        for(int i = len; i >= 0; i--) {
+            rl[i] = rlMul * nums[i];
+            rlMul = rl[i];
         }
 
         for(int i = 0; i <= len; i++) {
             if(!i) {
                 ans.push_back(rl[i + 1]);
             } else if(i == len) {
-                ans.push_back(lr[i - 1]);
+                ans.push_back(lrMul);
             } else {
-                ans.push_back(lr[i - 1] * rl[i + 1]);
+                ans.push_back(lrMul * rl[i + 1]);
             }
+            lrMul = lrMul * nums[i];
         }
 
         return ans;
