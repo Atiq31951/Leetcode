dca757520d0bab7906a2d711f783e424642d222b
atqrhmn <atiqur.rahman31951@gmail.com>
2026-09-06 17:23:42 +0600

    Time: 0 ms (100%), Space: 9.7 MB (75.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) {
-            return false;
-        }
-        
-        unordered_map<char, int> mp;
-        int totalChar = s.size();
+        if(s.length() != t.length()) return false;
 
-        for(int i = 0; i < s.size(); i++) {
-            mp[s[i]]++;
-            mp[t[i]]--;
+        unordered_map<char, int> mp1;
+
+        for(int i = 0; i < s.length(); i++) {
+            mp1[s[i]]++;
+            mp1[t[i]]--;
         }
 
-        for(auto m : mp) {
-            if(m.second != 0) {
-                return false;
-            }
+        for(auto x : mp1) {
+            if(x.second) return false;
         }
 
         return true;
